607b6b2ce527be1e43bbe221bf72b3646aa49b4e
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2019-12-03 02:49:09 +0100

    done

--- d6/code.R
+++ d6/code.R
@@ -36,6 +36,9 @@ X <- scale(X, center = T, scale = F)
 n <- nrow(X)
 #same for test:
 Ytest <- as.factor(testSet[,1])
+Ytest2 <- as.matrix(Y)
+Ytest3 <- as.matrix(testSet[,1])
+mode(Xtest) <- "double"
 Ytest <- as.matrix(dummy(Ytest))
 Xtest <- testSet[,-1]
 Xtest <- as.matrix(Xtest)
@@ -65,8 +68,8 @@ R_squared <- apply(com.xt,2,mean)
 Components <- data.frame(cbind("Components" = 1:rank, R_squared))
 
 ggplot(Components,aes(x=Components,y=R_squared)) + 
-  geom_point(shape=21) + geom_line() + theme_bw() + scale_x_continuous(breaks = c(1:9)) +
-  labs(y = expression(R^{2})) + ggtitle(expression(paste("Number of Components against  ", R^{2})))
+  geom_point(shape=15) + geom_line() + scale_x_continuous(breaks = c(1:9)) +
+  labs(y = expression(R^{2})) + ggtitle(expression(paste("Components against  ", R^{2})))
 
 
 ###############################################################################################
@@ -80,7 +83,7 @@ ggplot(Components,aes(x=Components,y=R_squared)) +
 
 
 #we choose 9 components
-model <- lm(as.matrix(Y) ~ TIB - 1)
+model <- lm(as.matrix(Ytest2) ~ TIB - 1)
 summaryModel <- summary(model)
 
 #train
@@ -96,27 +99,22 @@ IndividualAccuracy <- diag(t_cm)/colSums(t_cm)
 #        0         1         2         3         4         5         6         7         8         9 
 #0.9000000 1.0000000 0.8378378 0.9696970 0.7272727 0.7857143 0.7941176 0.7878788 0.8571429 0.8787879 
 #test
-Vxy <- var(Xtest,Ytest)
-aib <- eigen(t(Vxy)%*%Vxy)
-A <- Vxy %*% aib$vectors %*% diag(aib$values^(-0.5))
-A[,10] <- 0
-TIB <- as.matrix(Xtest) %*% A
-pred_test <- TIB %*% model$coefficients
+pred_test <- Xtest %*% A[,0:10] %*% model$coefficients
 pred_test_values <- unname(apply(pred_test,1,function(x) which.max(x)-1))
 
 #confusion matrix
 table(pred_test_values)
-table(Ytest2)
-t_cm <- table(pred_test_values,Ytest2)
+t_cm <- table(pred_test_values,Ytest3)
 rownames(t_cm) <- c(0:9)
 colnames(t_cm) <- c(0:9)
 t_cm
 t_accuracy <- sum(diag(t_cm))/sum(t_cm)
 IndividualAccuracy <- diag(t_cm)/colSums(t_cm)
 Accuracy <- round(c(IndividualAccuracy,"Model" = t_accuracy)*100,0)
-TSS = apply(Ytest,2,function(x){sum((x-mean(x))^2)})
-RSS = colSums((Ytest- pred_test)^2)
-R_sq_test = 100 - mean(RSS/TSS)
 
+TSS <- apply(Ytest,2,function(x){sum((x-mean(x))^2)})
+RSS <- colSums((Ytest- pred_test)^2)
+R_sq_test <- mean(1 - (RSS/TSS))
+R_sq_test
 
 
